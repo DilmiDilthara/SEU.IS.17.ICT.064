8db0f2cf15c0aedc767b463ff82509125e764c0c
dilmi <hadilmidilthara@gmail.com>
2021-06-16 12:40:57 +0530

    added new heading Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+
+<html>
+    <head>
+    </head>
+    <body>
+        <form>
+            <label for="fname">Name:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">User name:</label><br>
+            <input type="text" id="lname" name="lname"><br>
+            <label for="fname">Date of Birth:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">E mail:</label><br>
+            <input type="text" id="lname" name="lname"><br>
+            <label for="lname">NIC No:</label><br>
+
+            
+            <input type="text" id="lname" name="lname"><br>
+            <input type="radio" id="male" name="gender" value="male">
+            <label for="male">Male</label><br>
+            <input type="radio" id="female" name="gender" value="female">
+            <label for="female">Female</label><br>
+
+            <label for="pwd">Password:</label>
+            <input type="password" id="pwd" name="pwd"><br>
+
+            <label for="pwd">Re-Enter Password:</label>
+            <input type="password" id="pwd" name="pwd"><br>
+
+            
+            <input type="submit" value="Submit"><br>
+
+
+            
+            
+        </form>
+      
+        </body>  
+  
+ </html>
